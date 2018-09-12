210bcd6c2604748e78cbdd182375157c1f93c795
shenkai1999 <1786310402@qq.com>
2018-09-12 19:42:58 +0800

    第一次提交

---
+++ homework/program1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            string a = "";
+            int n = 0;
+            int d = 0;
+
+            Console.Write("Please input the first int:");
+            s = Console.ReadLine();
+            n = Int32.Parse(s);
+            Console.Write("Please input the second int:");
+            a = Console.ReadLine();
+            d = Int32.Parse(a);
+            Console.WriteLine("product=" + n + "*" + d + "=" + n * d);
+        }
+    }
+}
