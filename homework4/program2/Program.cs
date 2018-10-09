9ea1ac35f42f2d7a93d9647f8d8828c45ca65415
shenkai1999 <1786310402@qq.com>
2018-10-09 09:10:16 +0800

    第四次作业

--- homework4/program2/Program.cs
+++ homework4/program2/Program.cs
@@ -11,14 +11,36 @@ namespace program2
         static void Main(string[] args)
         {
             Order h = new Order();
-            OrderService.CreatLink(h, 2);//第二个参数设置初始订单的个数
-            Console.WriteLine("请输入新添加的订单的相关信息:");
-            OrderService.AddOrder(h);
-            OrderService.ShowOrder(h);
-            OrderService.DeleteOrder1(h, "001");
-            Console.WriteLine("删除后的订单列表为:");
-            OrderService.ShowOrder(h);
-            OrderService.SearchOrder(h, "002");
+            bool a = true;
+            
+            while (a)
+            {
+                Console.WriteLine("请选择你需要执行的功能:");
+                Console.WriteLine("1.建立订单链表");
+                Console.WriteLine("2.添加订单");
+                Console.WriteLine("3.显示订单");
+                Console.WriteLine("4.根据订单号查询订单");
+                Console.WriteLine("5.删除订单");
+                Console.WriteLine("6.退出");
+                string b = Console.ReadLine();
+                int c = Convert.ToInt32(b);
+                switch (c)
+                {
+                    case 1: OrderService.CreatLink(h, 1); break;//第二个参数设置初始订单的个数
+                    case 2: OrderService.AddOrder(h); break;
+                    case 3: OrderService.ShowOrder(h); break;
+                    case 4: OrderService.SearchOrder(h, "002"); break;
+                    case 5: OrderService.DeleteOrder1(h, "001"); break;
+                    case 6: a = false;break;
+                }
+            }
+            
+            
+            
+           
+            
+           
+            
         }
     }
 }
